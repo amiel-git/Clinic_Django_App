398ef9e2e6fbd37179b7ca68c5b3a628919aef84
Amiel <afurongamielchrysdin@gmail.com>
2021-01-22 22:54:16 +0800

    minor fixes for UpdateView

--- doctor/urls.py
+++ doctor/urls.py
@@ -6,7 +6,7 @@ app_name = 'doctor'
 urlpatterns = [
     path('list/',views.Doctor_listview.as_view(),name='doctor_list'),
     path('<slug:pk>/',views.Doctor_detailview.as_view(),name='doctor_detail'),
-    path('',views.create_doctor_view,name='register_doctor'),
+    path('',views.create_doctor_view,name='doctor_create'),
     path('delete/<slug:pk>/',views.Doctor_deleteview.as_view(),name='doctor_delete'),
     path('update/<slug:pk>',views.Doctor_updateview.as_view(),name='doctor_update'),
 ]
